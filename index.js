20011d3cba5fb773643beea5572e5ad9463295e2
rohiddev <rohiddev@gmail.com>
2020-09-29 09:24:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var http = require("http");
-var AWS = require('aws-sdk');
-var uuid = require('node-uuid');
+
 
 http.createServer(function (request, response) {
    // Send the HTTP header 
@@ -8,16 +7,7 @@ http.createServer(function (request, response) {
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
-   var s3 = new AWS.S3();
    
-   var params = {
-    Bucket: 'meh-ocp-dev-project-a', /* required */
-   };
-   
-   s3.listObjects(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-   });
    
    
    
